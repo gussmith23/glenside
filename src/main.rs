1797866a30f65fc93f85a8cfa013d11407ecc2bf
Gus Smith <guscomps@gmail.com>
2020-03-03 07:05:05 -0800

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use egg::{*, rewrite as rw};
+use egg::{rewrite as rw, *};
 
 define_language! {
     enum HardwareSoftwareLanguage {
@@ -16,14 +16,14 @@ fn main() {
         //rw!("break-for"; "(for ?index_var ?lower_bound ?upper_bound"),
         rw!("commute-add"; "(+ ?a ?b)" => "(+ ?b ?a)"),
         rw!("commute-mul"; "(* ?a ?b)" => "(* ?b ?a)"),
-
         rw!("add-0"; "(+ ?a 0)" => "?a"),
         rw!("mul-0"; "(* ?a 0)" => "0"),
         rw!("mul-1"; "(* ?a 1)" => "?a"),
     ];
 
-
-    let start = "(for i 0 4 (for j 0 4 (+ (index a i) (index b j))))".parse().unwrap();
+    let start = "(for i 0 4 (for j 0 4 (+ (index a i) (index b j))))"
+        .parse()
+        .unwrap();
 
     let (egraph, report) = SimpleRunner::default().run_expr(start, &hwswrules);
     println!(
