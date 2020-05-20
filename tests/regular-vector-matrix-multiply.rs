2dbf54e26242808cd1aad9b68036df18a40c64b3
Gus Smith <guscomps@gmail.com>
2020-05-20 09:33:45 -0700

    Add rewrite for bubbling concat through map dot prod (1/2)

--- tests/regular-vector-matrix-multiply.rs
+++ tests/regular-vector-matrix-multiply.rs
@@ -22,6 +22,7 @@ fn regular_vector_matrix_multiply() {
         rewrites::bubble_concat_through_cols_axis_1(),
         rewrites::bubble_concat_through_cartesian_product_not_last_axis_left(),
         rewrites::bubble_concat_through_cartesian_product_not_last_axis_right(),
+        rewrites::bubble_concat_through_map_dot_product_not_last_axis(),
     ];
 
     let (egraph, id) = egg::EGraph::<Language, Meta>::from_expr(&program);
