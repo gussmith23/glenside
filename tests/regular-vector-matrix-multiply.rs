9606dfec6a9efa83e37abd7feb25523b60a3fbbc
Gus Smith <guscomps@gmail.com>
2020-05-22 17:57:59 -0700

    Add comments

--- tests/regular-vector-matrix-multiply.rs
+++ tests/regular-vector-matrix-multiply.rs
@@ -3,8 +3,10 @@ use glenside::language::*;
 
 #[test]
 fn regular_vector_matrix_multiply() {
-
     // Our initial program: A 1x32 X 32x32 vector--matrix multiply.
+    // (v-32 is our vector, while t-32-32 is our matrix (or tensor))
+    // Our goal is to tile this program up into 1x16 X 16x16 computations and
+    // tensorize it to 16x16 systolic arrays. Let's go!
     let program = "
      (map-dot-product
       (cartesian-product
@@ -16,17 +18,38 @@ fn regular_vector_matrix_multiply() {
     .parse()
     .unwrap();
 
-    // All the rewrites needed to tensorize this program.
+    // We will achieve this tensorization with a set of program rewrites:
     let rws = vec![
         // These rewrites tile the program automatically.
+        // Specifically, they cut every tensor (including intermediate tensors!)
+        // in half and concatenate them back together, along both axes (0 and
+        // 1). Currently, we don't break down axes that are less than 16 in
+        // length, but we can adjust that later.
         rewrites::split(0, 16),
         rewrites::split(1, 16),
-
-        // This rewrite collapses multiple slice operators into one.
+        // This rewrite collapses multiple slice operators (introduced by the
+        // above rewrites) into one.
         rewrites::collapse_nested_slices(),
-
-        // These rewrites bubble concatenate operators up to the top of the
-        // program.
+        // These rewrites bubble concatenate operators (also introduced by the
+        // split rewrites) up to the top of the program.
+        // We need to get the concatenate operators to the top so that we can
+        // identify places where we can map in hardware.
+        // For example, if we see:
+        // (map-dot-product
+        //  (cartesian-product (concat ...) (concat ...))
+        // )
+        // ...we don't have a hardware atom that does this. But if we can
+        // rewrite it to:
+        // (concat
+        //  (map-dot-product
+        //   (cartesian-product <a 1x16 vector> <a 16x16 tensor>)
+        //  )
+        //  (map-dot-product
+        //   (cartesian-product <a 1x16 vector> <a 16x16 tensor>)
+        //  )
+        // )
+        // ...then we can map in systolic arrays for the (map-dot-product...)
+        // expressions, and the top-level concat will be handled by the compiler.
         rewrites::bubble_concat_through_rows_axis_0(),
         rewrites::bubble_concat_through_rows_axis_1(),
         rewrites::bubble_concat_through_cols_axis_0(),
@@ -36,8 +59,14 @@ fn regular_vector_matrix_multiply() {
         rewrites::bubble_concat_through_cartesian_product_last_axis(),
         rewrites::bubble_concat_through_map_dot_product_not_last_axis(),
         rewrites::bubble_concat_through_map_dot_product_last_axis(),
-
         // Finally, this rewrite tensorizes!
+        // It identifies patterns that we have hardware atoms for. Right now, it
+        // finds:
+        // (map-dot-product
+        //  (cartesian-product <a 1xrows vector> (cols <a rowsxcols tensor>))
+        // )
+        // and rewrites it to:
+        // (bsg-systolic-array <rows> <cols> < 1xrows vector> <a rowsxcols tensor>)
         rewrites::systolic_array_vector_matrix(),
     ];
 
