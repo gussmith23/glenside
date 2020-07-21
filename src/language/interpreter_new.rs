bbe2858bb884350766f0bc2bb8896f4bdeb3ff7f
Gus Smith <guscomps@gmail.com>
2020-07-21 11:20:54 -0700

    Move test out of unit tests

--- src/language/interpreter_new.rs
+++ src/language/interpreter_new.rs
@@ -325,12 +325,7 @@ where
 mod tests {
     use super::*;
     use ndarray::array;
-    use ndarray_npy::ReadableElement;
     use std::str::FromStr;
-
-    fn load_npy<DataType: ReadableElement>(path: &str) -> ndarray::ArrayD<DataType> {
-        ndarray_npy::read_npy::<_, ndarray::ArrayD<DataType>>(path).unwrap()
-    }
 
     #[test]
     fn compute_dot_product_0() {
@@ -634,64 +629,6 @@ mod tests {
             }) => {
                 assert_eq!(tensor, array![[1., 2.], [3., 4.]].into_dyn());
                 assert_eq!(access_axis, 0);
-            }
-            _ => panic!(),
-        }
-    }
-
-    #[test]
-    #[should_panic]
-    fn conv2d() {
-        // TODO(@gussmith) Support batch dimension
-        let expr = RecExpr::<Language>::from_str(
-            "
-         (compute dot-product
-          (access-cartesian-product
-           (access (access-tensor filters) 1)
-           (access-windows
-            activations
-            (slice-shape (shape-of filters) 1)
-            1
-            1
-           )
-          )
-         )
-        ",
-        )
-        .unwrap();
-
-        let filters = load_npy::<f32>(
-            format!(
-                "{}/{}",
-                env!("CARGO_MANIFEST_DIR"),
-                "data/conv2d_filters.npy"
-            )
-            .as_str(),
-        );
-        let activations = load_npy::<f32>(
-            format!(
-                "{}/{}",
-                env!("CARGO_MANIFEST_DIR"),
-                "data/conv2d_activations.npy"
-            )
-            .as_str(),
-        );
-        let result = load_npy::<f32>(
-            format!(
-                "{}/{}",
-                env!("CARGO_MANIFEST_DIR"),
-                "data/conv2d_result.npy"
-            )
-            .as_str(),
-        );
-
-        let mut env = Environment::new();
-        env.insert("filters", filters);
-        env.insert("activations", activations);
-
-        match interpret(&expr, expr.as_ref().len() - 1, &env) {
-            Value::Access(a) => {
-                assert_eq!(a.tensor, result);
             }
             _ => panic!(),
         }
