0afbc0931b185a38eef58a04360bb26e70ef0643
Gus Smith <guscomps@gmail.com>
2020-08-04 19:04:51 -0700

    Add ASPLOS demo as a subcommand of binary; run it in a test

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use clap::{App, Arg, SubCommand};
+use egg::{EGraph, RecExpr, Runner};
+use glenside::language::{Language, MyAnalysis};
+use serde_json::Value;
+use std::collections::HashMap;
+use std::io::Write;
+use std::str::FromStr;
+
+fn main() {
+    let matches = App::new("glenside")
+        .subcommand(
+            SubCommand::with_name("asplos-demo")
+                .arg(Arg::with_name("NAME").required(true).index(1))
+                .arg(Arg::with_name("PROGRAM").required(true).index(2))
+                .arg(Arg::with_name("SHAPES").required(true).index(3))
+                .arg(Arg::with_name("OUT_CODE_FILEPATH").required(true).index(4))
+                .arg(
+                    Arg::with_name("OUT_DESIGN_FILEPATH")
+                        .required(true)
+                        .index(5),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("asplos-demo") {
+        // Read in shapes as JSON dict; convert to HashMap<String, Vec<usize>>
+        let shapes_json: Value = serde_json::from_str(
+            std::fs::read_to_string(matches.value_of("SHAPES").unwrap())
+                .unwrap()
+                .as_str(),
+        )
+        .unwrap();
+        let mut shapes_map = HashMap::new();
+        for (name, value) in shapes_json.as_object().unwrap().iter() {
+            shapes_map.insert(
+                name.clone(),
+                value
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .map(|value| value.as_u64().unwrap() as usize)
+                    .collect::<Vec<_>>(),
+            );
+        }
+
+        // Read in program into egraph
+        let glenside_expr = RecExpr::<Language>::from_str(
+            std::fs::read_to_string(matches.value_of("PROGRAM").unwrap())
+                .unwrap()
+                .as_str(),
+        )
+        .unwrap();
+
+        let mut egraph = EGraph::new(MyAnalysis {
+            name_to_shape: shapes_map.clone(),
+        });
+        let id = egraph.add_expr(&glenside_expr);
+
+        let runner = Runner::default().with_egraph(egraph).run(&[
+            glenside::language::rewrites::collapse_nested_accesses(),
+            glenside::language::rewrites::collapse_nested_transposes(),
+            glenside::language::rewrites::remove_trivial_transpose(),
+            glenside::language::rewrites::systolic_array(),
+        ]);
+
+        let (_, extracted_expr) =
+            egg::Extractor::new(&runner.egraph, glenside::extraction::SimpleCostFunction)
+                .find_best(id);
+
+        let mut egraph = EGraph::new(MyAnalysis {
+            name_to_shape: shapes_map,
+        });
+        let id = egraph.add_expr(&extracted_expr);
+        let (hw_id_map, hw_atoms) = glenside::codegen::create_hardware_design_no_sharing(&egraph);
+
+        let code =
+            glenside::codegen::codegen(&egraph, id, &hw_id_map, matches.value_of("NAME").unwrap());
+
+        let json = glenside::hw_design_language::design_to_json(
+            &glenside::hw_design_language::HardwareDesign { atoms: hw_atoms },
+        );
+
+        std::fs::File::create(matches.value_of("OUT_CODE_FILEPATH").unwrap())
+            .unwrap()
+            .write_all(code.as_bytes())
+            .unwrap();
+        std::fs::File::create(matches.value_of("OUT_DESIGN_FILEPATH").unwrap())
+            .unwrap()
+            .write_all(serde_json::to_string_pretty(&json).unwrap().as_bytes())
+            .unwrap();
+    } else {
+        todo!()
+    }
+}
---
+++ tests/asplos-demo.rs
@@ -0,0 +1,93 @@
+use std::process::Command;
+
+#[test]
+fn asplos_demo_mlp() {
+    // The location of the Glenside program we'd like to compile.
+    let program_filepath = format!(
+        "{}/data/asplos-demo/mlp.glenside",
+        env!("CARGO_MANIFEST_DIR")
+    );
+
+    // This file holds shape information about the Glenside program.
+    // TODO(@gussmith23) This can be merged with the program itself.
+    let shapes_filepath = format!(
+        "{}/data/asplos-demo/mlp-shapes.json",
+        env!("CARGO_MANIFEST_DIR")
+    );
+
+    // Where we'll put the compiled C code.
+    let mut out_code_filepath = std::env::temp_dir();
+    out_code_filepath.push("mlp.c");
+
+    // Where we'll put the JSON hardware design.
+    let mut out_design_filepath = std::env::temp_dir();
+    out_design_filepath.push("hardware-design.json");
+
+    // Run Glenside!
+    let output = Command::new("target/debug/glenside")
+        // Subcommand of the glenside binary.
+        .arg("asplos-demo")
+        // Argument 1: the name of the function to compile.
+        // So our output will include a C function called mlp(...)
+        .arg("mlp")
+        .arg(program_filepath)
+        .arg(shapes_filepath)
+        .arg(&out_code_filepath)
+        .arg(out_design_filepath)
+        .output()
+        .expect("Failed to run glenside");
+
+    // Check that it ran.
+    assert!(
+        output.status.success(),
+        "Glenside binary failed with code {:?}. stderr:\n{}",
+        output.status.code(),
+        std::str::from_utf8(output.stderr.as_slice()).expect("Could not convert stderr to UTF8")
+    );
+
+    // Now, we'll test that the C code it generated actually works.
+
+    // The location of the test harness which tests the generated code.
+    let main_c_filepath = format!(
+        "{}/data/asplos-demo/mlp-test-harness.c",
+        env!("CARGO_MANIFEST_DIR")
+    );
+
+    // The location of the software implementation of a systolic array.
+    let systolic_array_impl_filepath = format!(
+        "{}/data/codegen-mlp/{}",
+        env!("CARGO_MANIFEST_DIR"),
+        "rtml_systolic_array_weight_stationary.c"
+    );
+
+    let output = Command::new("gcc")
+        .arg("-Werror")
+        // The test harness #includes the generated mlp.c file, so we include
+        // the tmp dir on the include path so that gcc will find mlp.c.
+        .arg(format!("-I{}", std::env::temp_dir().to_string_lossy()))
+        .arg(&main_c_filepath)
+        .arg(&systolic_array_impl_filepath)
+        .output()
+        .expect("Failed to compile main file with gcc");
+
+    // Check that it compiled.
+    assert!(
+        output.status.success(),
+        "Compilation failed. stderr:\n{}",
+        std::str::from_utf8(output.stderr.as_slice()).expect("Could not convert stderr to UTF8")
+    );
+
+    // Run the test harness.
+    let output = Command::new("./a.out")
+        .output()
+        .expect("Failed to run result");
+    assert!(
+        output.status.success(),
+        "Main binary failed with code {:?}. stderr:\n{}",
+        output.status.code(),
+        std::str::from_utf8(output.stderr.as_slice()).expect("Could not convert stderr to UTF8")
+    );
+
+    // To see the resulting C file and JSON hardware design, go to $TMPDIR/mlp.c
+    // and $TMPDIR/hardware-design.json.
+}
