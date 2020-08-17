bf2f6538c1d26e4d55d281220da5486499027204
Gus Smith <guscomps@gmail.com>
2020-08-17 10:33:54 -0700

    Use temp dir

--- tests/generate-mlp-allocate-for-manycore.rs
+++ tests/generate-mlp-allocate-for-manycore.rs
@@ -15,19 +15,29 @@ fn asplos_demo_generate_mlp_allocate_for_manycore() {
     running `cargo build`) to achieve the same results.
 
     */
-    std::process::Command::new("target/debug/glenside")
-        // Runs "asplos-demo" subcommand of Glenside
-        .arg("asplos-demo")
-        // Tells Glenside to allocate buffers for the Manycore
-        .arg("--allocate-for-manycore")
-        // The name of the resulting C function generated from the Glenside code
-        .arg("mlp")
-        // Input code files (Glenside code + tensor shape information)
-        .arg("data/asplos-demo/mlp.glenside")
-        .arg("data/asplos-demo/mlp-shapes.json")
-        // Output files (C code + hardware design)
-        .arg("out.c")
-        .arg("out.json")
-        .output()
-        .unwrap();
+    std::process::Command::new(format!(
+        "{}/target/debug/glenside",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+    .current_dir(std::env::temp_dir())
+    // Runs "asplos-demo" subcommand of Glenside
+    .arg("asplos-demo")
+    // Tells Glenside to allocate buffers for the Manycore
+    .arg("--allocate-for-manycore")
+    // The name of the resulting C function generated from the Glenside code
+    .arg("mlp")
+    // Input code files (Glenside code + tensor shape information)
+    .arg(format!(
+        "{}/data/asplos-demo/mlp.glenside",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+    .arg(format!(
+        "{}/data/asplos-demo/mlp-shapes.json",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+    // Output files (C code + hardware design)
+    .arg("out.c")
+    .arg("out.json")
+    .output()
+    .unwrap();
 }
