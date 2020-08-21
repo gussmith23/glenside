252d2e9cc3cdceca1086fb5965f716cf5148f45c
Gus Smith <guscomps@gmail.com>
2020-08-21 11:38:34 -0700

    Implement codegen for transpose

--- src/codegen.rs
+++ src/codegen.rs
@@ -7,6 +7,7 @@ use egg::Id;
 use itertools::Itertools;
 use ndarray::Dimension;
 use ndarray::IxDyn;
+use rand::Rng;
 use std::collections::HashMap;
 
 type Expr = EGraph<Language, MyAnalysis>;
@@ -37,6 +38,56 @@ pub fn create_allocation_str(prefix: &str, name: &str, shape: &[usize], dtype: D
         name,
         itertools::join(shape.iter().map(|dim: &usize| format!("[{}]", *dim)), "")
     )
+}
+
+/// Creates a representation (currently just a string with a C definition) of
+/// an allocation and an assignment.
+/// ```
+/// use glenside::hw_design_language::DType::Fp32;
+/// use glenside::codegen::create_assignment_str;
+/// assert_eq!(
+///     create_assignment_str(
+///         "my_prefix",
+///         "a",
+///         Fp32,
+///         &ndarray::ArrayD::from_shape_vec(vec![2, 3], (0..6).collect())
+///             .unwrap()
+///             .view()
+///     ),
+///     "my_prefix float a[2][3] = {{0, 1, 2}, {3, 4, 5}};"
+/// );
+///
+/// ```
+pub fn create_assignment_str<A: std::fmt::Display>(
+    prefix: &str,
+    name: &str,
+    dtype: DType,
+    array: &ndarray::ArrayViewD<A>,
+) -> String {
+    let mut allocation_string = create_allocation_str(prefix, name, array.shape(), dtype);
+    // TODO(@gussmith23) This is a hack
+    // Instead of this, create_allocation_str shouldn't return something with a
+    // ; at the end.
+    assert_eq!(allocation_string.chars().last().unwrap(), ';');
+
+    // Cut off the ;
+    allocation_string.truncate(allocation_string.len() - 1);
+
+    fn recursive_helper<A: std::fmt::Display>(array: &ndarray::ArrayViewD<A>) -> String {
+        if array.ndim() == 0 {
+            array.to_string()
+        } else {
+            format!(
+                "{{{}}}",
+                array
+                    .axis_iter(ndarray::Axis(0))
+                    .map(|a| recursive_helper(&a))
+                    .join(", ")
+            )
+        }
+    }
+
+    format!("{} = {};", allocation_string, recursive_helper(array))
 }
 
 /// Create a hardware design from an expression, creating a unique atom for each
@@ -355,8 +406,127 @@ fn codegen_recursive_helper(
             out_var_name
         }
         &Language::Usize(u) => format!("{}", u),
+        &Language::AccessTranspose([access_id, list_id]) => {
+            let access = match &expr[access_id].data {
+                MyAnalysisData::AccessPattern(a) => a,
+                _ => panic!(),
+            };
+            let original_shape = access
+                .shape
+                .slice()
+                .iter()
+                .chain(access.item_shape.slice().iter())
+                .cloned()
+                .collect::<Vec<_>>();
+
+            let new_axis_order = match &expr[list_id].data {
+                MyAnalysisData::List(l) => l,
+                _ => panic!(),
+            };
+
+            assert_eq!(original_shape.len(), new_axis_order.len());
+
+            let new_shape = new_axis_order
+                .iter()
+                .map(|i| original_shape[*i])
+                .collect::<Vec<_>>();
+
+            let access_var_name = codegen_recursive_helper(
+                expr,
+                access_id,
+                top_level_id,
+                allocations_prefix,
+                declarations,
+                code,
+                hw_map,
+            );
+
+            // Declare transpose output
+            // TODO(@gussmith23) Having to check this at every stage is not sustainable.
+            let transpose_out_var_name: String = if id == top_level_id {
+                "out".to_string()
+            } else {
+                // TODO(@gussmith23) Find a different way to name intermediates
+                // Currently generating random strings. Not great IMO.
+                let out = format!(
+                    "transpose_out_{}",
+                    rand::thread_rng()
+                        .sample_iter(&rand::distributions::Alphanumeric)
+                        .take(30)
+                        .collect::<String>()
+                );
+                declarations.push_str(
+                    create_allocation_str(
+                        allocations_prefix,
+                        out.as_str(),
+                        original_shape.as_slice(),
+                        DType::Fp32,
+                    )
+                    .as_str(),
+                );
+                out
+            };
+
+            let index_var_names = (0..original_shape.len())
+                .map(|i| format!("i{}", i))
+                .collect::<Vec<_>>();
+
+            // Create a for loop for every dimension in the shape.
+            for (dim_index, dim_len) in original_shape.iter().enumerate() {
+                let index_var_name = &index_var_names[dim_index];
+                code.push_str(
+                    format!(
+                        "
+int {};
+for ({} = 0; {} < {}; {}++) {{",
+                        index_var_name, index_var_name, index_var_name, dim_len, index_var_name
+                    )
+                    .as_str(),
+                );
+            }
+
+            let index_var_names_reordered = new_axis_order
+                .iter()
+                .map(|i| &index_var_names[*i])
+                .collect::<Vec<_>>();
+
+            // Within the innermost for loop: assign to the output at the
+            // correct location.
+            // We have indices i0..i(n-1), for each of the n axes.
+            code.push_str(
+                format!(
+                    "
+{}[{}] = {}[{}];",
+                    transpose_out_var_name,
+                    (0..original_shape.len())
+                        .map(|i| format!(
+                            "{}*({})",
+                            index_var_names_reordered[i],
+                            new_shape[i + 1..].iter().product::<usize>()
+                        ))
+                        .collect::<Vec<_>>()
+                        .join(" + "),
+                    access_var_name,
+                    (0..original_shape.len())
+                        .map(|i| format!(
+                            "{}*({})",
+                            index_var_names[i],
+                            original_shape[i + 1..].iter().product::<usize>()
+                        ))
+                        .collect::<Vec<_>>()
+                        .join(" + ")
+                )
+                .as_str(),
+            );
+
+            // Close each for loop
+            for _ in original_shape.iter() {
+                code.push_str("}");
+            }
+
+            transpose_out_var_name
+        }
         &Language::GetAccessShape(_)
-        | &Language::AccessTranspose(_)
         | Language::List(_)
         | &Language::AccessBroadcast(_)
         | &Language::AccessInsertAxis(_)
@@ -386,5 +556,117 @@ fn codegen_recursive_helper(
         | &Language::AccessSlice(_)
         | &Language::AccessConcatenate(_)
         | &Language::AccessShiftRight(_) => panic!("{:#?} not implemented", expr[id].nodes[0]),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use egg::RecExpr;
+    use std::fs::File;
+    use std::io::Write;
+    use std::process::Command;
+    use std::str::FromStr;
+
+    #[test]
+    fn transpose() {
+        let shape = vec![1, 20, 300, 3];
+        let permutation = vec![3, 1, 0, 2];
+        let input = ndarray::ArrayD::from_shape_vec(
+            shape.clone(),
+            (0..shape.iter().product::<usize>()).collect(),
+        )
+        .unwrap();
+        let input_transposed = input.clone().permuted_axes(permutation.clone());
+
+        let expr = RecExpr::from_str(
+            format!(
+                "
+(access-transpose (access-tensor t) (list {}))",
+                permutation.iter().map(|x| x.to_string()).join(" ")
+            )
+            .as_str(),
+        )
+        .unwrap();
+
+        let mut map = HashMap::default();
+        map.insert("t".to_string(), shape.clone());
+
+        let mut egraph = EGraph::new(MyAnalysis { name_to_shape: map });
+        let id = egraph.add_expr(&expr);
+
+        let code = codegen(&egraph, id, &HashMap::default(), "transpose", "");
+
+        let main_code = format!(
+            "
+#include <assert.h>
+
+{}
+{}
+{}
+{}
+
+int main() {{
+  transpose(&out[0], &a[0]);
+
+  int i;
+  for (i = 0; i < {}; i++) {{
+    assert(((float*)a_t)[i] == ((float*)out)[i]);
+  }}
+}}
+",
+            create_assignment_str("", "a", DType::Fp32, &input.view()),
+            create_assignment_str("", "a_t", DType::Fp32, &input_transposed.view()),
+            create_assignment_str(
+                "",
+                "out",
+                DType::Fp32,
+                &ndarray::ArrayD::<f32>::zeros(input_transposed.shape()).view()
+            ),
+            code,
+            shape.iter().product::<usize>()
+        );
+
+        println!("{}", main_code);
+
+        let main_c_filepath = std::env::temp_dir().with_file_name(format!(
+            "transpose-test-{}.c",
+            std::time::SystemTime::now().elapsed().unwrap().as_nanos()
+        ));
+
+        let binary_filepath = std::env::temp_dir().with_file_name(format!(
+            "transpose-test-{}",
+            std::time::SystemTime::now().elapsed().unwrap().as_nanos()
+        ));
+        println!("{}", binary_filepath.to_string_lossy());
+
+        File::create(&main_c_filepath)
+            .unwrap()
+            .write_all(main_code.as_bytes())
+            .unwrap();
+
+        let result = Command::new("gcc")
+            .arg("-g")
+            .arg("-o")
+            .arg(&binary_filepath)
+            .arg(&main_c_filepath)
+            .output()
+            .unwrap();
+
+        assert!(
+            result.status.success(),
+            "{}",
+            std::str::from_utf8(result.stderr.as_slice())
+                .expect("Could not convert stderr to UTF8")
+        );
+
+        let result = Command::new(&binary_filepath).output().unwrap();
+
+        assert!(
+            result.status.success(),
+            "{}",
+            std::str::from_utf8(result.stderr.as_slice())
+                .expect("Could not convert stderr to UTF8")
+        );
     }
 }
