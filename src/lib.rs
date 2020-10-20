4fce0033491a91a955cd703c7eac21ddcd701e5e
Gus Smith <guscomps@gmail.com>
2020-10-20 11:14:40 -0700

    Clean up cplex feature flag a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 #![feature(test)]
+//! ## Glenside Feature Flags
+//!
+//! Glenside generally leans towards *enabling* all features by default, purely
+//! because it makes testing on my machine (where I have all of the dependencies
+//! set up) easier. To disable glenside features, you should use
+//! `--no-default-features` and enable features manually.
+//!
 //! Parts of Glenside (namely extraction; see [`glenside::extraction`]) rely on
 //! IBM's CPLEX solver. You can disable these components by not enabling
 //! Glenside's `cplex` feature.
