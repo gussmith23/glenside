5cafa436f215d2342585cd934b00a995be76c2b7
AD1024 <dh63@cs.washington.edu>
2021-11-02 23:32:27 -0700

    [ add ] nn.pad compilation support

--- src/extraction/mod.rs
+++ src/extraction/mod.rs
@@ -71,6 +71,7 @@ impl egg::CostFunction<Language> for MonolithicCostFunction<'_> {
             | Language::SystolicArray(_)
             | Language::SystolicArrayWithBlocking(_)
             | Language::Usize(_)
+            | Language::Int32(_)
             | Language::ConstructTuple(_)
             | Language::TupleGetItem(_)
             | Language::AccessSlice(_)
@@ -222,7 +223,7 @@ impl CostFunction<Language> for SimpleCostFunction {
             | AccessBroadcast(_) => 1,
             // Other glenside constructs that are necessary.
             Shape(_) | ShapeOf(_) | SliceShape(_) | ShapeInsertAxis(_) | ShapeRemoveAxis(_)
-            | List(_) | AccessShape(_) | Usize(_) | PadType(_) | ComputeType(_) | Symbol(_)
+            | List(_) | AccessShape(_) | Usize(_) | Int32(_) | PadType(_) | ComputeType(_) | Symbol(_)
             | Literal(_) | NotNanFloat64(_) => 1,
             // Old constructs that are no longer used
             MoveAxis(_) | CartesianProduct(_) | MapDotProduct(_) | Slice(_) | Concatenate(_)
@@ -258,8 +259,8 @@ impl CostFunction<Language> for AcceleratorCostFunction {
             | Language::Symbol(_)
             | Language::RelayOperatorCall(_)
             | Language::PadType(_)
+            | Language::Int32(_)
             | Language::AccessTensor(_) => 0,
-            Language::RelayOperatorCall(_) => 0,
             Language::RelayOperator(op) => match op {
                 crate::language::RelayOperator::RelayReshape
                 | crate::language::RelayOperator::RelayBatchFlatten => 1,
