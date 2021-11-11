7774111ea1faa3897e46a28d4d2c88aab7736711
AD1024 <dh63@cs.washington.edu>
2021-11-10 22:29:14 -0800

    fix compilation

--- src/language/language.rs
+++ src/language/language.rs
@@ -1695,7 +1695,7 @@ impl egg::Analysis<Language> for MyAnalysis {
                             MyAnalysisData::AccessPattern(a) => a.clone(),
                             _ => panic!(),
                         };
-        
+
                         assert_eq!(access.item_shape.ndim(), 2);
                         assert_eq!(access.shape.ndim(), 0);
                         let t = access.item_shape[0];
@@ -1704,7 +1704,7 @@ impl egg::Analysis<Language> for MyAnalysis {
                         assert_eq!(h % 16, 0);
                         access.item_shape[0] = access.item_shape[0] / 2;
                         access.contains_accelerator_calls = true;
-        
+
                         MyAnalysisData::AccessPattern(access)
                     }
                     crate::language::AcceleratorFunc::HlsCNNConv2D => {
@@ -2101,7 +2101,7 @@ impl egg::Analysis<Language> for MyAnalysis {
                                         assert_eq!(group, channels);
                                         assert_eq!(kernel_size.shape[0], *channels);
 
-                                        let mut weight_shape = weight
+                                        let weight_shape = weight
                                             .shape
                                             .slice()
                                             .iter()
