0b6100429e328af7de9af5ff859ee41acaa9e58f
Gus Smith <guscomps@gmail.com>
2021-11-19 11:34:37 -0800

    Add comments

--- tests/transformer.rs
+++ tests/transformer.rs
@@ -44,4 +44,56 @@ fn transformer() {
             _ => panic!(),
         }
     );
+
+    // Currently, these checks won't work, as merge() is not fully working. Mike
+    // has a hack where he manually figures this out later.
+
+    // let runner = Runner::default()
+    //     .with_egraph(egraph)
+    //     .with_node_limit(1000000)
+    //     .run(vec![&glenside::language::rewrites::dot_product_with_vta()]);
+    // runner.print_report();
+
+    // assert!(
+    //     "(accelerator-call vta-dense ?x ?w ?shape)"
+    //         .parse::<Pattern<_>>()
+    //         .unwrap()
+    //         .search(&runner.egraph)
+    //         .len()
+    //         > 0
+    // );
+
+    // let matches ="(accelerator-call vta-dense ?x ?w ?shape)"
+    //         .parse::<Pattern<_>>()
+    //         .unwrap()
+    //         .search(&runner.egraph);
+    // assert!(matches.len() > 0);
+    // println!("{:#?}", &runner.egraph[matches[0].eclass]);
+    // assert!(matches.iter().all(|m| match &runner.egraph[m.eclass].data {
+    //     MyAnalysisData::AccessPattern(a) => a.contains_accelerator_calls,
+    //     _ => panic!(),
+    // }));
+
+    // let matches ="(access-insert-axis (accelerator-call vta-dense ?x ?w ?shape) 0)"
+    //         .parse::<Pattern<_>>()
+    //         .unwrap()
+    //         .search(&runner.egraph);
+    // assert!(matches.len() > 0);
+    // println!("{:#?}", &runner.egraph[matches[0].eclass]);
+    // println!("{:#?}", &runner.egraph[runner.egraph[matches[0].eclass].nodes[0].children()[0]]);
+    // assert!(matches.iter().any(|m| match &runner.egraph[m.eclass].data {
+    //     MyAnalysisData::AccessPattern(a) => a.contains_accelerator_calls,
+    //     _ => panic!(),
+    // }));
+
+    // let matches ="(access-concatenate (access-insert-axis (accelerator-call vta-dense ?x ?w ?shape) 0) ?second ?dim)"
+    //         .parse::<Pattern<_>>()
+    //         .unwrap()
+    //         .search(&runner.egraph);
+    // assert!(matches.len() > 0);
+    // println!("{:#?}", &runner.egraph[matches[0].eclass]);
+    // assert!(matches.iter().all(|m| match &runner.egraph[m.eclass].data {
+    //     MyAnalysisData::AccessPattern(a) => a.contains_accelerator_calls,
+    //     _ => panic!(),
+    // }));
 }
