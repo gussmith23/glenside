5252c55e0b4448fe7fcf1ff9eb709db8d44b69ce
Gus Smith <guscomps@gmail.com>
2021-12-28 16:34:48 -0800

    dense -> dense+biasadd rewrite

--- src/codegen.rs
+++ src/codegen.rs
@@ -698,6 +698,7 @@ fn codegen_helper(
             };
 
             match relay_op {
+                RelayOperator::RelayZeros => todo!(),
                 RelayOperator::RelayBatchMatmul => todo!(),
                 RelayOperator::RelayLayerNorm => todo!(),
                 RelayOperator::RelayRound => todo!(),
