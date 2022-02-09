be18d616832a25eca4386f9970ec992f35edf661
Vishal Canumalla <vishalc@cs.washington.edu>
2022-02-08 16:13:34 -0800

    ssd_r34 relay file

---
+++ tests/ssd_r34_relay_to_glenside.rs
@@ -0,0 +1,58 @@
+#![cfg(feature = "tvm")]
+
+use egg::EGraph;
+use glenside::language::MyAnalysis;
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+// Mobilenet, simplified for inference (so batch norms are simplified).
+// Generate with:
+// ```python3
+// import tvm
+// from tvm import relay
+// from tvm.relay.testing.mobilenet import get_workload
+//
+// mod, _ = get_workload()
+// mod = relay.transform.SimplifyInference()(mod)
+// print(mod.astext())
+// ```
+
+// Mobilenet
+// Generate with:
+// ```python3
+// import tvm
+// from tvm import relay
+// from tvm.relay.testing.mobilenet import get_workload
+//
+// mod, _ = get_workload()
+// print(mod.astext())
+// ```
+#[test]
+fn parse_ssd_r34() {
+    let filename = PathBuf::from(format!(
+        "{}/models/ssd_r34.relay",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+    let relay = std::fs::read_to_string(&filename).unwrap();
+    let module = tvm::ir::module::IRModule::parse("", relay).unwrap();
+
+    let (expr, shapes_vec) = glenside::language::from_relay::from_relay(
+        &module,
+        true,
+        &vec![],
+    );
+
+    let mut env = HashMap::default();
+    for (k, v) in &shapes_vec {
+        env.insert(k.clone(), v.clone());
+    }
+
+    // TODO(@gussmith23) Include some simple simplifying rewrites
+    // If we add some very basic rewrites here, then $glenside_str
+    // won't need to exactly match what's actually produced by
+    // from_relay.py. It can be simpler (e.g. collapsing accesses).
+    let mut egraph = EGraph::new(MyAnalysis {
+        name_to_shape: env.clone(),
+    });
+    egraph.add_expr(&expr);
+}
