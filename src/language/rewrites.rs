121182c7053b74b726cd8ee5f0cdfd2fe2e0ebb5
Gus Smith <guscomps@gmail.com>
2022-02-23 10:39:33 -0800

    Finish test

--- src/language/rewrites.rs
+++ src/language/rewrites.rs
@@ -5069,9 +5069,8 @@ mod tests {
 
         let runner = Runner::<_, _, ()>::new(MyAnalysis::default())
             .with_egraph(egraph)
+            .with_iter_limit(7)
             .run(&rws);
-
-        runner.print_report();
 
         let matches = "
          (access-reshape
@@ -5098,13 +5097,7 @@ mod tests {
         .unwrap()
         .search_eclass(&runner.egraph, id)
         .unwrap();
-        //assert_eq!(matches.substs.len(), 1);
-
-        for subst in matches.substs {
-            println!("{:?}", runner.egraph[subst["?print-me".parse().unwrap()]]);
-        }
-
-        todo!();
+        assert_eq!(matches.substs.len(), 1);
     }
 
     #[test]
