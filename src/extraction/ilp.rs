d474dc31604478fb2739a611b5614fb0abe8a3e6
AD1024 <dh63@cs.washington.edu>
2022-02-23 10:42:03 -0800

    [ add ] constraint for choosing parents of a selected eclass

--- src/extraction/ilp.rs
+++ src/extraction/ilp.rs
@@ -390,6 +390,36 @@ pub fn create_generic_egraph_lp_model<'a>(
             problem.add_constraint(con).unwrap();
         }
 
+        // If an eclass is selected, then at least one of its parents must be selected
+        // if all its parents are filtered out, then this eclass must not be selected
+        let mut available_parents = vec![];
+        for p in egraph[*id].parents.iter() {
+            if let Some(parent_idx) = bn_vars.get(&p.0) {
+                available_parents.push(parent_idx);
+            }
+        }
+
+        if available_parents.len() == 0 {
+            let mut con = Constraint::new(ConstraintType::Eq,
+                0.0,
+                format!("Disable eclass {} because it doesn't have an available parent", id));
+            con.add_wvar(WeightedVariable::new_idx(*bq_idx, 1.0));
+            problem.add_constraint(con).unwrap();
+            continue;
+        } else {
+            // bq => OR p_idx for p_idx in bq of eclass parents
+            let mut con = Constraint::new(
+                ConstraintType::GreaterThanEq,
+                0.0,
+                format!("Need to choose parents for {}", id),
+            );
+            con.add_wvar(WeightedVariable::new_idx(*bq_idx, -1.0));
+            for p_idx in available_parents.into_iter() {
+                con.add_wvar(WeightedVariable::new_idx(*p_idx, 1.0));
+            }
+            problem.add_constraint(con).unwrap();
+        }
+
         // If an enode is selected, then its child eclasses must be selected.
         // Implemented as
         // -bn + bq >= 0 for each bq
