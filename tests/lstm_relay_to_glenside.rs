a9a5321394b90fe94620637757d993be3c2ab49d
Gus Smith <guscomps@gmail.com>
2022-02-23 10:46:18 -0800

    Continue work

--- tests/lstm_relay_to_glenside.rs
+++ tests/lstm_relay_to_glenside.rs
@@ -87,39 +87,46 @@ fn lstm_relay_to_glenside() {
             ],
         );
 
-        // let pattern_ast = RecExpr::from(
-        //     orig_pattern
-        //         .as_ref()
-        //         .iter()
-        //         .map(|enode| {
-        //             if let crate::Language::Symbol(name) = enode {
-        //                 ENodeOrVar::Var(Var::from_str(&format!("?{}", name)).unwrap())
-        //             } else {
-        //                 // Construct the ENode-type node in the pattern AST by first
-        //                 // recursively converting the children of this node.
-        //                 ENodeOrVar::ENode(enode.clone())
-        //             }
-        //         })
-        //         .collect::<Vec<_>>(),
-        // );
-
-        // Here, we don't use any Vars. This means we won't bind anything with
-        // this pattern, BUT the pattern should be much faster according to Max.
         let pattern_ast = RecExpr::from(
             orig_pattern
                 .as_ref()
                 .iter()
-                .map(|enode| ENodeOrVar::ENode(enode.clone()))
+                .map(|enode| {
+                    // We have a single Var in this pattern: it's the "%x"
+                    // argument to the pattern. In the pattern compiled to
+                    // Glenside, it looks like (access-tensor x).
+                    if let crate::Language::AccessTensor(id) = enode {
+                        if let crate::Language::Symbol(v) = &orig_pattern[*id] {
+                            if v == "x" {
+                                return ENodeOrVar::Var(Var::from_str("?x".into()).unwrap());
+                            }
+                        }
+                    }
+                    // Construct the ENode-type node in the pattern AST by first
+                    // recursively converting the children of this node.
+                    ENodeOrVar::ENode(enode.clone())
+                })
                 .collect::<Vec<_>>(),
         );
+
+        // Here, we don't use any Vars. This means we won't bind anything with
+        // this pattern, BUT the pattern should be much faster according to Max.
+        // let pattern_ast = RecExpr::from(
+        //     orig_pattern
+        //         .as_ref()
+        //         .iter()
+        //         .map(|enode| ENodeOrVar::ENode(enode.clone()))
+        //         .collect::<Vec<_>>(),
+        // );
 
         Pattern::from(pattern_ast)
     };
 
+    println!("{}", pattern.pretty(80));
     assert_eq!(pattern.search(&egraph).len(), 1);
 
-    let rewrite = rewrite!("flexlstm"; 
-        { pattern } => "(accelerator-call flexlstm x hidden0 hidden1 rnn_weight_ih_l0 rnn_weight_hh_l0 rnn_bias_ih_l0 rnn_bias_hh_l0)");
+    let rewrite = rewrite!("flex-lstm"; 
+        { pattern } => "(accelerator-call flex-lstm x hidden0 hidden1 rnn_weight_ih_l0 rnn_weight_hh_l0 rnn_bias_ih_l0 rnn_bias_hh_l0)");
 
     let runner = Runner::default().with_egraph(egraph).run(vec![&rewrite]);
 
@@ -134,6 +141,7 @@ fn lstm_relay_to_glenside() {
      (construct-tuple ?a ?b)"
         .parse::<Pattern<_>>()
         .unwrap()
-        .search_eclass(&runner.egraph, id).unwrap();
+        .search_eclass(&runner.egraph, id)
+        .unwrap();
     assert_eq!(matches.substs.len(), 1);
 }
